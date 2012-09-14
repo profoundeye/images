fdcc4e98b645f7d61bc08568b0f73b815540f782
zplaying <profoundeye@gmail.com>
2012-09-14 11:17:25 +0800

    初次提交。上线时需要修正config中的配置

---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'debug' => true,
+    //'error_redirect' => 'http://avnpc.com/pages/evacloudimage',
+    'libPath' => __DIR__ . '/lib',  //PHPthumb library path
+    'sourceRootPath' => __DIR__ . '/../attachs',  //original image save path, require path read permission
+    'thumbFileRootPath' => __DIR__ . '/../attachs/thumb', //resized thumbnails save path, require read and write permission
+    'thumbUrlRootPath' => __DIR__ . '/..' , //thumbnails url root path, require read and write permission
+    'saveImage' => true,  //if true, thumbnails will be created and auto save as same directory structure as original images
+	'dir'=>'testyunbian/images/',//如果当前文件不是在根目录。根目录名
+);
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * EvaCloudImage
+ * light-weight url based image transformation php library
+ *
+ * @link      https://github.com/AlloVince/EvaCloudImage
+ * @copyright Copyright (c) 2012 AlloVince (http://avnpc.com/)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @author    AlloVince
+ */
+
+
+
+require_once 'EvaCloudImage.php';
+error_reporting(E_ALL);
+
+// Check php version
+if( version_compare(phpversion(), '5.3.0', '<') ) {
+    printf('PHP 5.3.0 is required, you have %s', phpversion());
+    exit(1);
+}
+
+$cloudImage = new EvaCloudImage(null, include 'config.inc.php');
+$cloudImage->show();
